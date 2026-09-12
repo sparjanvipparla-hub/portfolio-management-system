50239600f20ac1f761cd83eb74b0e6897f5a0589
SPARJAN <sparjanvipparla@gmail.com>
2026-09-12 09:42:53 +0530

    Initial commit

---
+++ src/main/java/com/portfolio/app/Main.java
@@ -0,0 +1,20 @@
+package com.portfolio.app;
+
+import com.portfolio.app.model.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("===============================");
+        System.out.println("Portfolio Management Project");
+        System.out.println("===============================");
+        
+        // Demo user values with rishi
+        User user = new User("user-1234", "rishi", "rishi@gmail.com");
+        
+        System.out.println("User ID: " + user.getUserid());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Email: " + user.getEmail());
+    }
+
+}
---
+++ src/main/java/com/portfolio/app/model/User.java
@@ -0,0 +1,44 @@
+package com.portfolio.app.model;
+
+public class User {
+    // 1. Fields
+    private String userid;
+    private String name;
+    private String email;
+
+    // 2. Default Constructor
+    public User() {
+    }
+
+    // 3. Parameterized Constructor
+    public User(String userid, String name, String email) {
+        this.userid = userid;
+        this.name = name;
+        this.email = email;
+    }
+
+    // 4. Getter and Setter Methods
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
